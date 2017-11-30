77b8d811880b42c24f8fb8e367d0d81f317209c1
Xetzer <xetzer@gmail.com>
2017-11-30 16:33:19 +0300

    change conditions to install python

--- homework1/install.sh
+++ homework1/install.sh
@@ -4,22 +4,28 @@ echo "Installing dependencies:"
 sudo yum install -y zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel xz xz-devel
 curl -L https://raw.github.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 echo "Adding variables:"
-if ! grep -q 'export PYENV_ROOT="$HOME/.pyenv"' ~/.bash_profile; then
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+if ! grep -q 'export PYENV_ROOT="$HOME/.pyenv"' ~/.bashrc; then
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 fi
-if ! grep -q 'export PATH="$PYENV_ROOT/bin:$PATH"' ~/.bash_profile; then
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+if ! grep -q 'export PATH="$PYENV_ROOT/bin:$PATH"' ~/.bashrc; then
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 fi
-if ! grep -q 'eval "$(pyenv init -)"' ~/.bash_profile; then
-echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+if ! grep -q 'eval "$(pyenv init -)"' ~/.bashrc; then
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 fi
-if ! grep -q 'eval "$(pyenv virtualenv-init -)"' ~/.bash_profile; then
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
+if ! grep -q 'eval "$(pyenv virtualenv-init -)"' ~/.bashrc; then
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 fi
-source ~/.bash_profile
+source ~/.bashrc
 echo "Installing python:"
+if ! [ -d  ~/.pyenv/versions/2.7.14/ ]; then
+echo "Installing python 2.7.14"
 pyenv install 2.7.14
+fi
+if ! [ -d ~/.pyenv/versions/3.5.4/ ]; then
+echo "Installing python 3.5.4"
 pyenv install 3.5.4
+fi
 echo "Available python vesrions:"
 pyenv versions
 echo "Creating environments:"
@@ -29,4 +35,3 @@ pyenv global 3.5.4
 pyenv virtualenv venv3.5
 echo "Available environments:"
 pyenv virtualenvs
-
