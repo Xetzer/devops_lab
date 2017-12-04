51e958c628ac19345d9a925d656ecafb3ae7646e
Xetzer <xetzer@gmail.com>
2017-12-04 12:23:59 +0300

    task 4 - add restrictions

--- homework2/4.py
+++ homework2/4.py
@@ -1,11 +1,36 @@
 from collections import defaultdict
 d = defaultdict(list)
-n, m = map(int, input("Please enter n and m separated by space:").split())
+
+while True:
+    try:
+        n, m = map(int, input("Please enter n and m separated by space:").split())
+        if n < 1 or n > 10000 or m < 1 or m > 100:
+            raise ValueError
+        break
+    except ValueError:
+        print("This values are incorrect, try again.")
 
 for i in range(n):
-    d['A'].append(input())
+    while True:
+        try:
+            a = (input())
+            if len(a) > 100:
+                raise ValueError
+            break
+        except ValueError:
+            print("This word is too long")
+    d['A'].append(a)
 for j in range(m):
-    d['B'].append(input())
+    while True:
+        try:
+            b = (input())
+            if len(b) > 100:
+                raise ValueError
+            break
+        except ValueError:
+            print("This word is too long")
+    d['B'].append(b)
+
 listA = list(d.get('A'))
 listB = list(d.get('B'))
 
