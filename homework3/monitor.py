c7e0dbb4d92497dc8f564f02c3bbc16a807c6f96
Xetzer <xetzer@gmail.com>
2017-12-04 19:08:13 +0300

    create script for task 3

---
+++ homework3/monitor.py
@@ -0,0 +1,40 @@
+"""Simple system monitor app"""
+from datetime import datetime
+from time import sleep
+import psutil
+try:
+    import configparser
+except ImportError:
+    import ConfigParser as configparser
+
+CONFIG = configparser.ConfigParser()
+CONFIG.read("settings.ini")
+OUTP = "output." + CONFIG.get("common", "output")
+PER = int(CONFIG.get("common", "interval"))*60
+NUMB = int(CONFIG.get("common", "snap_num"))
+
+def stats():
+    """Function to get system stats"""
+    while True:
+        global NUMB
+        snap = "SNAPSHOT " + str(NUMB)
+        dat = "TIMESTAMP:" + str(datetime.now().strftime("%Y-%m-%d,%H:%M:%S"))
+        cpu = "CPU usage,%:" + str(psutil.cpu_percent(interval=1, percpu=False))
+        mem = "Memory usage,%:" + str(psutil.virtual_memory().percent)
+        virt = "VirMem usage,%:" + str(psutil.swap_memory().percent)
+        diskr = "Disk read,Mb:" + str(round(psutil.disk_io_counters().read_bytes/(1024**2), 2))
+        diskw = "Disk write,Mb:" + str(round(psutil.disk_io_counters().write_bytes / (1024 ** 2), 2))
+        disku = "Disk usage,%:" + str(psutil.disk_usage('/').percent)
+        netws = "Network sent,Mb:" + str(round(psutil.net_io_counters().bytes_sent / (1024 ** 2), 2))
+        netwr = "Network received,Mb:" + str(round(psutil.net_io_counters().bytes_recv / (1024 ** 2), 2))
+        print("{0}: {1} {2} {3} {4} {5} {6} {7} {8} {9}".format(snap, dat, cpu, mem, virt, diskr, diskw, disku, netws, netwr))
+        with open(OUTP, "a") as log:
+            log.write("{0}: {1} {2} {3} {4} {5} {6} {7} {8} {9}\n".format(snap, dat, cpu, mem, virt, diskr, diskw, disku, netws, netwr))
+            log.close()
+        NUMB += 1
+        CONFIG.set("common", "snap_num", str(NUMB))
+        with open("settings.ini", "w") as conffile:
+            CONFIG.write(conffile)
+            conffile.close()
+        sleep(PER)
+stats()
