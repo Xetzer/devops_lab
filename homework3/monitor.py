6225d6c3ca8ba99c99f6570101fdf3a78590af6e
Xetzer <xetzer@gmail.com>
2017-12-06 09:52:09 +0300

    change variables names

--- homework3/monitor.py
+++ homework3/monitor.py
@@ -7,17 +7,17 @@ try:
 except ImportError:
     import ConfigParser as configparser
 
-CONFIG = configparser.ConfigParser()
-CONFIG.read("settings.ini")
-OUTP = "output." + CONFIG.get("common", "output")
-PER = int(CONFIG.get("common", "interval"))*60
-NUMB = int(CONFIG.get("common", "snap_num"))
+config = configparser.ConfigParser()
+config.read("settings.ini")
+outp = "output." + config.get("common", "output")
+per = int(config.get("common", "interval"))*60
+numb = int(config.get("common", "snap_num"))
 
 def stats():
     """Function to get system stats"""
     while True:
-        global NUMB
-        snap = "SNAPSHOT " + str(NUMB)
+        global numb
+        snap = "SNAPSHOT " + str(numb)
         dat = "TIMESTAMP:" + str(datetime.now().strftime("%Y-%m-%d,%H:%M:%S"))
         cpu = "CPU usage,%:" + str(psutil.cpu_percent(interval=1, percpu=False))
         mem = "Memory usage,%:" + str(psutil.virtual_memory().percent)
@@ -28,13 +28,13 @@ def stats():
         netws = "Network sent,Mb:" + str(round(psutil.net_io_counters().bytes_sent / (1024 ** 2), 2))
         netwr = "Network received,Mb:" + str(round(psutil.net_io_counters().bytes_recv / (1024 ** 2), 2))
         print("{0}: {1} {2} {3} {4} {5} {6} {7} {8} {9}".format(snap, dat, cpu, mem, virt, diskr, diskw, disku, netws, netwr))
-        with open(OUTP, "a") as log:
+        with open(outp, "a") as log:
             log.write("{0}: {1} {2} {3} {4} {5} {6} {7} {8} {9}\n".format(snap, dat, cpu, mem, virt, diskr, diskw, disku, netws, netwr))
             log.close()
-        NUMB += 1
-        CONFIG.set("common", "snap_num", str(NUMB))
+        numb += 1
+        config.set("common", "snap_num", str(numb))
         with open("settings.ini", "w") as conffile:
-            CONFIG.write(conffile)
+            config.write(conffile)
             conffile.close()
-        sleep(PER)
+        sleep(per)
 stats()
