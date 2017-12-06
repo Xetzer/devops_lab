52604b49321d23bd6a2b9383a94f7fc3838c93fa
Xetzer <xetzer@gmail.com>
2017-12-06 10:53:55 +0300

    change variables names

--- homework4/mon_class.py
+++ homework4/mon_class.py
@@ -7,17 +7,17 @@ try:
 except ImportError:
     import ConfigParser as configparser
 
-CONFIG = configparser.ConfigParser()
-CONFIG.read("settings.ini")
-OUTP = "output." + CONFIG.get("common", "output")
-PER = int(CONFIG.get("common", "interval"))*60
-NUMB = int(CONFIG.get("common", "snap_num"))
+config = configparser.ConfigParser()
+config.read("settings.ini")
+outp = "output." + config.get("common", "output")
+per = int(config.get("common", "interval"))*60
+numb = int(config.get("common", "snap_num"))
 
 class System_Mon:
     '''Class that collects system information'''
     def __init__(self):
-        global NUMB
-        self.snap = "SNAPSHOT " + str(NUMB)
+        global numb
+        self.snap = "SNAPSHOT " + str(numb)
         self.dat = "TIMESTAMP:" + str(datetime.now().strftime("%Y-%m-%d,%H:%M:%S"))
         self.cpu = "CPU usage,%:" + str(psutil.cpu_percent(interval=1, percpu=False))
         self.mem = "Memory usage,%:" + str(psutil.virtual_memory().percent)
@@ -32,14 +32,15 @@ class System_Mon:
         print("{0}: {1} {2} {3} {4} {5} {6} {7} {8} {9}".format(self.snap, self.dat, self.cpu, self.mem, self.virt, self.diskr, self.diskw, self.disku, self.netws, self.netwr))
     def WriteStats(self):
         '''Method that writes current system state to log'''
-        global NUMB
-        with open(OUTP, "a") as log:
-            log.write("{0}: {1} {2} {3} {4} {5} {6} {7} {8} {9}\n".format(self.snap, self.dat, self.cpu, self.mem, self.virt, self.diskr, self.diskw, self.disku, self.netws, self.netwr))
+        global numb
+        with open(outp, "a") as log:
+            log.write("{0}: {1} {2} {3} {4} {5} {6} {7} {8} {9}\n".format(self.snap, self.dat, self.cpu, self.mem, self.virt, \
+                                                                          self.diskr, self.diskw, self.disku, self.netws, self.netwr))
             log.close()
-        NUMB += 1
-        CONFIG.set("common", "snap_num", str(NUMB))
+        numb += 1
+        config.set("common", "snap_num", str(numb))
         with open("settings.ini", "w") as conffile:
-            CONFIG.write(conffile)
+            config.write(conffile)
             conffile.close()
 
 def out():
@@ -50,4 +51,4 @@ def out():
 
 while True:
     out()
-    sleep(PER)
+    sleep(per)
