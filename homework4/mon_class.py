e3807d539d48829e6d57c3181d7a651bfa4577d3
Xetzer <xetzer@gmail.com>
2017-12-09 15:11:03 +0300

    add some required changes

--- homework4/mon_class.py
+++ homework4/mon_class.py
@@ -1,4 +1,4 @@
-"""Simple system monitor app"""
+'''Collecting system stats'''
 from datetime import datetime
 from time import sleep
 import psutil
@@ -9,44 +9,46 @@ except ImportError:
 
 config = configparser.ConfigParser()
 config.read("settings.ini")
-outp = "output." + config.get("common", "output")
-per = int(config.get("common", "interval"))*60
-numb = int(config.get("common", "snap_num"))
+output = "output." + config.get("common", "output")
+period = int(config.get("common", "interval"))*60
 
-class System_Mon:
+class SystemMonitor:
     '''Class that collects system information'''
-    def __init__(self):
-        global numb
-        self.snap = "SNAPSHOT " + str(numb)
-        self.dat = "TIMESTAMP:" + str(datetime.now().strftime("%Y-%m-%d,%H:%M:%S"))
+    def __init__(self, number):
+        self.number = number
+        self.date = "TIMESTAMP:" + str(datetime.now().strftime("%Y-%m-%d,%H:%M:%S"))
         self.cpu = "CPU usage,%:" + str(psutil.cpu_percent(interval=1, percpu=False))
-        self.mem = "Memory usage,%:" + str(psutil.virtual_memory().percent)
-        self.virt = "VirMem usage,%:" + str(psutil.swap_memory().percent)
-        self.diskr = "Disk read,Mb:" + str(round(psutil.disk_io_counters().read_bytes / (1024 ** 2), 2))
-        self.diskw = "Disk write,Mb:" + str(round(psutil.disk_io_counters().write_bytes / (1024 ** 2), 2))
-        self.disku = "Disk usage,%:" + str(psutil.disk_usage('/').percent)
-        self.netws = "Network sent,Mb:" + str(round(psutil.net_io_counters().bytes_sent / (1024 ** 2), 2))
-        self.netwr = "Network received,Mb:" + str(round(psutil.net_io_counters().bytes_recv / (1024 ** 2), 2))
-    def GetStats(self):
+        self.memory = "Memory usage,%:" + str(psutil.virtual_memory().percent)
+        self.virtual = "VirMem usage,%:" + str(psutil.swap_memory().percent)
+        self.disk_read = "Disk read,Mb:" + str(round(psutil.disk_io_counters().read_bytes / (1024 ** 2), 2))
+        self.disk_write = "Disk write,Mb:" + str(round(psutil.disk_io_counters().write_bytes / (1024 ** 2), 2))
+        self.disk_usage = "Disk usage,%:" + str(psutil.disk_usage('/').percent)
+        self.net_sent = "Network sent,Mb:" + str(round(psutil.net_io_counters().bytes_sent / (1024 ** 2), 2))
+        self.net_received = "Network received,Mb:" + str(round(psutil.net_io_counters().bytes_recv / (1024 ** 2), 2))
+
+    def get_stats(self):
         '''Method that prints current system state'''
-        print("{0}: {1} {2} {3} {4} {5} {6} {7} {8} {9}".format(self.snap, self.dat, self.cpu, self.mem, self.virt, self.diskr, self.diskw, self.disku, self.netws, self.netwr))
-    def WriteStats(self):
+        print("SNAPSHOT {0}: {1} {2} {3} {4} {5} {6} {7} {8} {9}".format(self.number, self.date, self.cpu, self.memory,
+                                                                         self.virtual, self.disk_read, self.disk_write,
+                                                                         self.disk_usage, self.net_sent, self.net_received))
+    def write_stats(self):
         '''Method that writes current system state to log'''
-        global numb
-        with open(outp, "a") as log:
-            log.write("{0}: {1} {2} {3} {4} {5} {6} {7} {8} {9}\n".format(self.snap, self.dat, self.cpu, self.mem, self.virt, \
-                                                                          self.diskr, self.diskw, self.disku, self.netws, self.netwr))
-        numb += 1
-        config.set("common", "snap_num", str(numb))
-        with open("settings.ini", "w") as conffile:
-            config.write(conffile)
+        with open(output, "a") as log:
+            log.write("SNAPSHOT {0}: {1} {2} {3} {4} {5} {6} {7} {8} {9}\n".format(self.number, self.date, self.cpu, self.memory,
+                                                                                   self.virtual, self.disk_read, self.disk_write,
+                                                                                   self.disk_usage, self.net_sent, self.net_received))
+        self.number += 1
+        config.set("common", "snap_num", str(self.number))
+        with open("settings.ini", "w") as config_file:
+            config.write(config_file)
 
 def out():
-    '''Method that treates new object of System_Mon class'''
-    new_snapshot = System_Mon()
-    new_snapshot.GetStats()
-    new_snapshot.WriteStats()
+    '''Method that creates new object of System_Mon class'''
+    number = int(config.get("common", "snap_num"))
+    new_snapshot = SystemMonitor(number)
+    new_snapshot.get_stats()
+    new_snapshot.write_stats()
 
 while True:
     out()
-    sleep(per)
+    sleep(period)
--- homework4/test.py
+++ homework4/test.py
@@ -9,48 +9,48 @@ except ImportError:
 
 config = configparser.ConfigParser()
 config.read("settings.ini")
-outp = "output." + config.get("common", "output")
-per = int(config.get("common", "interval"))*60
-numb = int(config.get("common", "snap_num"))
+output = "output." + config.get("common", "output")
+period = int(config.get("common", "interval"))*60
 
-class System_Mon:
+class SystemMonitor:
     '''Class that collects system information'''
-    def __init__(self):
-        global numb
-        self.snap = "SNAPSHOT " + str(numb)
-        self.dat = "TIMESTAMP:" + str(datetime.now().strftime("%Y-%m-%d,%H:%M:%S"))
+    def __init__(self, number):
+        self.number = number
+        self.date = "TIMESTAMP:" + str(datetime.now().strftime("%Y-%m-%d,%H:%M:%S"))
         self.cpu = "CPU usage,%:" + str(psutil.cpu_percent(interval=1, percpu=False))
-        self.mem = "Memory usage,%:" + str(psutil.virtual_memory().percent)
-        self.virt = "VirMem usage,%:" + str(psutil.swap_memory().percent)
-        self.diskr = "Disk read,Mb:" + str(round(psutil.disk_io_counters().read_bytes / (1024 ** 2), 2))
-        self.diskw = "Disk write,Mb:" + str(round(psutil.disk_io_counters().write_bytes / (1024 ** 2), 2))
-        self.disku = "Disk usage,%:" + str(psutil.disk_usage('/').percent)
-        self.netws = "Network sent,Mb:" + str(round(psutil.net_io_counters().bytes_sent / (1024 ** 2), 2))
-        self.netwr = "Network received,Mb:" + str(round(psutil.net_io_counters().bytes_recv / (1024 ** 2), 2))
+        self.memory = "Memory usage,%:" + str(psutil.virtual_memory().percent)
+        self.virtual = "VirMem usage,%:" + str(psutil.swap_memory().percent)
+        self.disk_read = "Disk read,Mb:" + str(round(psutil.disk_io_counters().read_bytes / (1024 ** 2), 2))
+        self.disk_write = "Disk write,Mb:" + str(round(psutil.disk_io_counters().write_bytes / (1024 ** 2), 2))
+        self.disk_usage = "Disk usage,%:" + str(psutil.disk_usage('/').percent)
+        self.net_sent = "Network sent,Mb:" + str(round(psutil.net_io_counters().bytes_sent / (1024 ** 2), 2))
+        self.net_received = "Network received,Mb:" + str(round(psutil.net_io_counters().bytes_recv / (1024 ** 2), 2))
 
-class FormSnap(System_Mon):
+class FormatSnapshot(SystemMonitor):
     '''Class that prints current system state and writes it to log'''
-    def __init__(self):
-        super().__init__()
-    def PrintStats(self):
+    def print_stats(self):
         '''Method that prints current system state'''
-        print("{0}: {1} {2} {3} {4} {5} {6} {7} {8} {9}".format(self.snap, self.dat, self.cpu, self.mem, self.virt, self.diskr, self.diskw, self.disku, self.netws, self.netwr))
-    def WriteStats(self):
+        print("SNAPSHOT {0}: {1} {2} {3} {4} {5} {6} {7} {8} {9}".format(self.number, self.date, self.cpu, self.memory,
+                                                                         self.virtual, self.disk_read, self.disk_write,
+                                                                         self.disk_usage, self.net_sent, self.net_received))
+    def write_stats(self):
         '''Method that writes current system state to log'''
-        global numb
-        with open(outp, "a") as log:
-            log.write("{0}: {1} {2} {3} {4} {5} {6} {7} {8} {9}\n".format(self.snap, self.dat, self.cpu, self.mem, self.virt, self.diskr, self.diskw, self.disku, self.netws, self.netwr))
-        numb += 1
-        config.set("common", "snap_num", str(numb))
-        with open("settings.ini", "w") as conffile:
-            config.write(conffile)
+        with open(output, "a") as log:
+            log.write("SNAPSHOT {0}: {1} {2} {3} {4} {5} {6} {7} {8} {9}\n".format(self.number, self.date, self.cpu, self.memory,
+                                                                                   self.virtual, self.disk_read, self.disk_write,
+                                                                                   self.disk_usage, self.net_sent, self.net_received))
+        self.number += 1
+        config.set("common", "snap_num", str(self.number))
+        with open("settings.ini", "w") as config_file:
+            config.write(config_file)
 
 def out():
-    '''Function that treates new object of FormSnap class'''
-    new_snapshot = FormSnap()
-    new_snapshot.PrintStats()
-    new_snapshot.WriteStats()
+    '''Function that creates new object of FormatSnapshot class'''
+    number = int(config.get("common", "snap_num"))
+    new_snapshot = FormatSnapshot(number)
+    new_snapshot.print_stats()
+    new_snapshot.write_stats()
 
 while True:
     out()
-    sleep(per)
+    sleep(period)
